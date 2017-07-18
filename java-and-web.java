7866e95d3be6d885743bbe8479010f83aa78784e
arrizaqu <arrizaqu@yahoo.com>
2017-07-18 08:36:20 +0700

    add assigment to bootcamp 114

---
+++ java-and-web.java
@@ -0,0 +1,23 @@
+# DAO Pattern dan Java Web
+
+1. DAO (Definition)
+2. Buat Create, Read, Update, delete, untuk table xsis_employee, xsis_customer, dan xsis_user dengan menerapkan DAO (example)
+3. Javascript ? (definisi dan Example)
+4. Bagaimana cara javascript bekerja ? (defenisi)
+5. Jquery ? (definisi)
+6. sebutkan keuntungan dan mengapa harus menggunakan jquery (definisi)
+7. hello world, jQUERY (example)
+8. apa maksud dari $(document).ready(function(){}) ?
+9. apa perbedaan dari $(document).ready(function(){}) vs $(function(){}) ?
+10. sebutkan macam - macam jquery selector dan berikan contohnya masing (example)
+11. Berikan contoh untuk jquery ajax ?
+12. berikan contoh untuk jquery ajax menggunakan Java ?
+13. buat data untuk table html dengan cara sebagai berikut : 
+	a. buat form input 
+	b. buat table html
+	c. buat data dari no.a dan langsung insert ke table no.b
+13. silahkan untuk memasang dan install datatable dari libaray berikut : 
+	a. situs library: https://datatables.net/
+	b. buat form di html dan insert ke table tersebut (tanpa menggunakan database dan tanpa refresh)
+14. modifikasi fasilitas searching dari library tersebut ?
+15. apa itu JSON, dan berikan contohnya ? (definisi dan example)
